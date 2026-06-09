fe3f96bd9d5509f4ff91f5d62dd98b51d0f5f910
Way-cfg <Way-cfg@://github.com>
2026-06-09 11:58:58 +0100

    Bump version to v0.1.1

--- src/components/DockSidebar.tsx
+++ src/components/DockSidebar.tsx
@@ -126,7 +126,7 @@ export default function DockSidebar() {
         </nav>
 
         <div className="mt-auto px-3 shrink-0">
-          <span className="text-[10px] text-white/12">v0.1.0</span>
+          <span className="text-[10px] text-white/15 font-mono">v0.1.1</span>
         </div>
       </motion.div>
     </aside>
